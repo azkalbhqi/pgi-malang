a584e21afa3e3ef8247a542209a3ee5b47315027
azkalbhqi <azkalpredator@gmail.com>
2025-01-21 11:44:54 +0700

    setup deploy 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/flowbite.min.js',],
+            
             refresh: true,
             mode: 'production',  // Pastikan mode tidak undefined
         }),
